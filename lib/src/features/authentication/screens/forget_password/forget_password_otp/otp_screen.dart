696a7b456a686263f7a31615b505f0e43f42b29c
HoussemDAAS <houssemdaas2@gmail.com>
2024-04-01 23:15:47 +0100

    some changes made

--- lib/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart
+++ lib/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart
@@ -9,50 +9,52 @@ class OTPScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(tDefaultSize),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                tOtpTitle,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              Text(
-                tOtpSubTitle.toUpperCase(),
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleSmall,
-              ),
-              const SizedBox(height: 40.0),
-              Text(
-                tOtpMessage,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.displaySmall,
-              ),
-              const SizedBox(height: 20.0),
-              OtpTextField(
-                numberOfFields: 6,
-                
-                fillColor: tPrimaryColor.withOpacity(0.1),
-                 focusedBorderColor: tPrimaryColor,
-                filled: true,
-                keyboardType: TextInputType.number,
-                onSubmit: (code){
-                 
-                },
-              ),
-              const SizedBox(height: 20.0),
-               SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    
-                  },
-                  child: Text("Vérifier".toUpperCase()),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.all(tDefaultSize),
+              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  tOtpTitle,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge,
                 ),
-              ),
-            ])),
+                Text(
+                  tOtpSubTitle.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(height: 40.0),
+                Text(
+                  tOtpMessage,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                const SizedBox(height: 20.0),
+                OtpTextField(
+                  numberOfFields: 6,
+                  
+                  fillColor: tPrimaryColor.withOpacity(0.1),
+                   focusedBorderColor: tPrimaryColor,
+                  filled: true,
+                  keyboardType: TextInputType.number,
+                  onSubmit: (code){
+                   
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                 SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      
+                    },
+                    child: Text("Vérifier".toUpperCase()),
+                  ),
+                ),
+              ])),
+        ),
       ),
     );
   }
--- lib/src/features/authentication/screens/forget_password/forget_password_phone/forget_password_phone.dart
+++ lib/src/features/authentication/screens/forget_password/forget_password_phone/forget_password_phone.dart
@@ -12,61 +12,63 @@ class ForgetPasswordPhoneScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(tDefaultSize),
-          child: Column(
-            children:   [
-            const  SizedBox(
-               height: tDefaultSize*4,
-             ),
-            const  LoginHeader(
-              image: tForget,
-              title: tPhone,
-              subtitle: "Merci d'enter votre Numéro de Telephone",
-              crossAxisAlignment: CrossAxisAlignment.center,
-              heightBetween: 30.0,
-              textAlign: TextAlign.center,
-              
-             ),
-            const  SizedBox(
-               height: tFormHeight,
+    return  SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(tDefaultSize),
+            child: Column(
+              children:   [
+              const  SizedBox(
+                 height: tDefaultSize*4,
                ),
-               Form(child: Column(
-                children: [
-                 const  TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline_outlined),
-                  labelText: "Phone",
-                  hintText: " Numéro de Telephone",
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: tPrimaryColor, width: 2.0),
+              const  LoginHeader(
+                image: tForget,
+                title: tPhone,
+                subtitle: "Merci d'enter votre Numéro de Telephone",
+                crossAxisAlignment: CrossAxisAlignment.center,
+                heightBetween: 30.0,
+                textAlign: TextAlign.center,
+                
+               ),
+              const  SizedBox(
+                 height: tFormHeight,
+                 ),
+                 Form(child: Column(
+                  children: [
+                   const  TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline_outlined),
+                    labelText: "Phone",
+                    hintText: " Numéro de Telephone",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: tPrimaryColor, width: 2.0),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
+                    labelStyle: TextStyle(color: tPrimaryColor),
                   ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  ),
-                  labelStyle: TextStyle(color: tPrimaryColor),
+                ),
+                 const   SizedBox(height: tFormHeight),
+                 SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.to(()=>const OTPScreen());
+                  },
+                  child: Text("Envoyer".toUpperCase()),
                 ),
               ),
-               const   SizedBox(height: tFormHeight),
-               SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.to(()=>const OTPScreen());
-                },
-                child: Text("Envoyer".toUpperCase()),
-              ),
-            ),
-                ],
-               ),
-               )
-        
-             
-            ],
-          ),
+                  ],
+                 ),
+                 )
           
+               
+              ],
+            ),
+            
+          ),
         ),
       ),
     );
