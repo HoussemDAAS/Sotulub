51bf7836eb20386e9614162624edb3b643f8ab66
HoussemDAAS <houssemdaas2@gmail.com>
2024-04-15 16:47:07 +0100

    slider done but border radius is not assigned

--- lib/src/constants/image_string.dart
+++ lib/src/constants/image_string.dart
@@ -2,3 +2,7 @@ const String tSplashImage = "assets/images/oilMen.png";
 const String tSplashImageTop="assets/images/oil.png";
 const String tWelcomeScreen="assets/images/sotulub.png";
 const String tForget="assets/images/forget.png";
+const String tSlider1="assets/images/slider1.png";
+const String tSlider2="assets/images/slider2.png";
+const String tSlider3="assets/images/slider3.png";
+const String tSlider4="assets/images/slider4.png";
--- lib/src/features/core/screens/dashboard/widgets/dashboard.dart
+++ lib/src/features/core/screens/dashboard/widgets/dashboard.dart
@@ -1,12 +1,56 @@
+import 'dart:async';
+
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+import 'package:sotulub/src/common_widgets/slider/promoSlider.dart';
+
 import 'package:sotulub/src/constants/colors.dart';
+
 import 'package:sotulub/src/constants/sizes.dart';
 import 'package:sotulub/src/constants/text_strings.dart';
-import 'package:sotulub/src/features/authentication/screens/charts/line_charts.dart';
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key});
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+
+  updatePageIndicator(int index) {}
+}
+
+class _DashboardState extends State<Dashboard> {
+  bool _showToast = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Show the toast when the page is opened
+    _showToast = true;
+
+    // Schedule the appearance of the toast after 20 seconds if it's closed
+    Timer(Duration(seconds: 20), () {
+      if (!_showToast) {
+        setState(() {
+          _showToast = true;
+        });
+      }
+    });
+  }
+
+  void _hideToast() {
+    setState(() {
+      _showToast = false;
+    });
+
+    // Schedule the appearance of the toast after 20 seconds
+    Timer(Duration(seconds: 20), () {
+      setState(() {
+        _showToast = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,16 +77,46 @@ class Dashboard extends StatelessWidget {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(tDashboardPadding),
-          child: Column(
-            children: [
-              LineChartSample(), // Displaying the LineChartSample widget here
-            ],
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.only(
+                top: tDashboardPadding * 1.5, // Adjust this factor as needed
+                left: 10,
+                right: 10,
+                bottom: tDashboardPadding,
+              ),
+              child: TpromoSlider(),
+            ),
           ),
-        ),
+          if (_showToast)
+            Positioned(
+              top: 10,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                color: Color(0xFFA6E9D2), // Custom background color
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Demande de convention est en cours",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.close, color: Colors.white),
+                      onPressed: _hideToast,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
 }
+
