feacbcc20d48ce59a4a1a264bafa30cf90f8999f
HoussemDAAS <houssemdaas2@gmail.com>
2024-06-13 21:57:28 +0100

    login changes

--- lib/src/features/authentication/screens/forget_password/forget_password_options/forget_password_bottom_sheet.dart
+++ lib/src/features/authentication/screens/forget_password/forget_password_options/forget_password_bottom_sheet.dart
@@ -3,18 +3,24 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:sotulub/src/constants/sizes.dart';
 import 'package:sotulub/src/constants/text_strings.dart';
-import 'package:sotulub/src/features/authentication/screens/forget_password/forget_password_mail/forget_password_mail.dart';
 import 'package:sotulub/src/features/authentication/screens/forget_password/forget_password_options/forget_password_btn_widget.dart';
-import 'package:sotulub/src/features/authentication/screens/forget_password/forget_password_phone/Forget_password_phone.dart';
 import 'package:sotulub/src/utils/theme/text_theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ForgetPasswordBottomSheet extends StatelessWidget {
   const ForgetPasswordBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-       padding: EdgeInsets.all(tDefaultSize),
+    final Uri phoneNumber = Uri.parse('tel:+216-71-861-234');
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: 'admin@gmail.com',
+      query: 'subject=Mot de passe Reset&body=Merci de reinitialiser ma mot de passe',
+    );
+
+    return Container(
+      padding: EdgeInsets.all(tDefaultSize),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -26,28 +32,38 @@ class ForgetPasswordBottomSheet extends StatelessWidget {
             tForgetPasswordSubTitle,
             style: TTextTheme.lightTextTheme.displayMedium,
           ),
-        const   SizedBox(
+          const SizedBox(
             height: 30.0,
           ),
           ResetPasswordOption(
             icon: Icons.mail_outline_rounded,
-            title: tEmail,
-            subTitle: tRestViaEmail,
-            onTap: () {
+            title: 'Par Email',
+            subTitle: 'admin@gmail.com',
+            onTap: () async {
               Navigator.pop(context);
-              Get.to(()=> const ForgetPasswordMailScreen());
+              if (await canLaunch(emailUri.toString())) {
+                await launch(emailUri.toString());
+              } else {
+                Get.snackbar('Erreur', 'Impossible d\'ouvrir l\'application de messagerie.',
+                    snackPosition: SnackPosition.BOTTOM);
+              }
             },
           ),
-        const   SizedBox(
+          const SizedBox(
             height: 30.0,
           ),
           ResetPasswordOption(
             icon: Icons.mobile_friendly_rounded,
-            title: tPhone,
-            subTitle: tResetViaPhone,
-            onTap: () {
+            title: 'Par Téléphone',
+            subTitle: '(+216) 71 86 12 34',
+            onTap: () async {
               Navigator.pop(context);
-              Get.to(()=> const ForgetPasswordPhoneScreen());
+              if (await canLaunch(phoneNumber.toString())) {
+                await launch(phoneNumber.toString());
+              } else {
+                Get.snackbar('Erreur', 'Impossible d\'ouvrir l\'application de téléphone.',
+                    snackPosition: SnackPosition.BOTTOM);
+              }
             },
           ),
         ],
